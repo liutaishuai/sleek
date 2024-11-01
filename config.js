cd192ab1ef07e195e97f3e6ae1a88dcf8d7b9992
liutaishuai <117056247+liutaishuai@users.noreply.github.com>
2024-11-01 22:21:20 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.config={
     theme:true,
     
     // 后端网址配置
-    hosturl:"http://test2.d1w.top/",
+    hosturl:"http://test2.d1w.top",
     title:"test加速器",
     title1:"快速高效与世界连接",
     title2:"快速,可靠,信赖",
